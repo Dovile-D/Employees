2a9d4f5a55de7b814d82a9cfa648c3db005f787e
Dovile-D <dovile.d.git@gmail.com>
2022-06-08 12:50:44 +0300

    add global variables

--- src/main/java/EmployeeDAO.java
+++ src/main/java/EmployeeDAO.java
@@ -1,3 +1,8 @@
 public class EmployeeDAO {
+    // global variables:
+    private static final String DATABASE_NAME = "employee_db";
+    private static final String DATABASE_USERNAME = "root";
+    private static final String DATABASE_PASSWORD = "";
+
 
 }
