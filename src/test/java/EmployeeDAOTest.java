20be784bd1e0f3a2d328fa0c5df1720a0ac9d791
Dovile-D <dovile.d.git@gmail.com>
2022-06-08 22:55:34 +0300

    add EmployeeDAO JUnit test

--- src/test/java/EmployeeDAOTest.java
+++ src/test/java/EmployeeDAOTest.java
@@ -1,2 +1,27 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class EmployeeDAOTest {
+    Employee localEmployee;
+    @Before
+    public void setUp() {
+        localEmployee = new Employee("Ugne", "Barauskaite", 5891.56);
+    }
+    @Test
+    public void createNewEntryTest() {
+        EmployeeDAO.create(localEmployee);
+        ArrayList<Employee> remoteEmployee = EmployeeDAO.searchByName("Ugne");
+        // remoteEmployee is an ArrayList so comparing by index:
+        compareEmployees(localEmployee, remoteEmployee.get(0));
+    }
+
+    public void compareEmployees(Employee localEmployee, Employee remoteEmployee) {
+        Assert.assertEquals(localEmployee.getName(), remoteEmployee.getName());
+        Assert.assertEquals(localEmployee.getSurname(), remoteEmployee.getSurname());
+//        Assert.assertEquals(localEmployee.getSalary(), remoteEmployee.getSalary());
+    }
+
 }
