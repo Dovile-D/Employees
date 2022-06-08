55275a1d3d95123fc42b9a274393d3263ec5be72
Dovile-D <dovile.d.git@gmail.com>
2022-06-08 12:37:01 +0300

    add Employee.java

---
+++ src/main/java/Employee.java
@@ -0,0 +1,69 @@
+public class Employee {
+    private int id;
+    private String name;
+    private String surname;
+    private double salary;
+
+    // constructor for editing an entry:
+    public Employee(int id, String name, String surname, double salary) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    // constructor for adding a new entry:
+    public Employee(String name, String surname, double salary) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+    }
+
+    // an empty constructor:
+    public Employee() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
+
+
